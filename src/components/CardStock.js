3dc345b075dab3bc39ad9ab9c534de15f15c691f
Douglas Passos <douglas.p.doug@gmail.com>
2023-05-19 21:18:04 -0300

    Ajuste no Card / inclusao de informações e ajuste no layout

--- src/components/CardStock.js
+++ src/components/CardStock.js
@@ -1,9 +1,23 @@
 import './CardStock.css';
 
 function CardStock(props){
+  const data = props.data;
+
   return (
     <div className='card'>
-      {props.texto}
+      <div className='card-alta'>
+        <p>Alta: <span>99,99</span></p>
+
+      </div>
+      <div className='card-fecha'>
+        <p>Close: <span>{data.close}</span></p>
+      </div>
+      <div className='card-baixa'>
+        <p>Baixa: <span>00,00</span></p>
+      </div>
+      <div className='card-empresa'>
+        <p>{data.name}</p><img alt={`logo ${data.name}`}  src={data.logo}></img>
+      </div>
     </div>
   );
 }
