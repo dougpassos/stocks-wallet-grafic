3d80dab8ae76074096ba89cfbd896a7e54201c70
Douglas Passos <douglas.p.doug@gmail.com>
2023-05-19 21:19:24 -0300

    Ajuste e objeto com dados para utilizar e testar o card

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import CardStock from './components/CardStock';
 import NavBar from './components/NavBar';
 
 function App() {  
+  const stockData = {
+    "stock": "PETR3F",
+    "name": "PETROBRAS",
+    "close": 29.18,
+    "change": -0.06849657,
+    "volume": 20544,
+    "market_cap": 364644976258,
+    "logo": "https://s3-symbol-logo.tradingview.com/brasileiro-petrobras--big.svg",
+    "sector": "Energy Minerals"
+  } 
+
+
   return (
     <div className="App">
       <header className="App-header">
         <NavBar></NavBar>      
       </header>
-      <CardStock texto='Douglas'></CardStock>
+      <CardStock data={stockData}></CardStock>
     </div>
   );
 }
