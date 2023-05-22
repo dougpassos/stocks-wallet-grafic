5f125a4a1d1de7d9cec36a1be92d2da86160f125
Douglas Passos <douglas.p.doug@gmail.com>
2023-05-21 23:09:04 -0300

    service getDataTIcket

--- src/services/stocks-services.js
+++ src/services/stocks-services.js
@@ -6,17 +6,36 @@ const configAxios = axios.create({
     "Content-Type": "application/json",
   },
 });
-/*
+
+
 const getTicketList = async(stockQuery) => {
   try {
     const response = await configAxios.get(`/quote/list?sortBy=close&sortOrder=desc&limit=5&search=${stockQuery}`);
-    return await response.data
-   
-    
+    return await response.data    
+  } 
+  catch (error) {
+    console.log(error);
+  }
+};
+
+const getDataTicket = async(ticket,
+                            range='5d',
+                            interval='5d',
+                            fund=true,
+                            divid=false
+                            ) => {
+  try {
+    const response = await configAxios.get(
+      `/quote/${ticket}
+        ?range=${range}
+        &interval=${interval}
+        &fundamental=${fund}
+        &dividends=${divid}`
+    ).then(response => response.json());
+    return response;    
   } catch (error) {
     console.log(error);
   }
+}
 
-};*/
-
-export default configAxios;
+export default getTicketList;
