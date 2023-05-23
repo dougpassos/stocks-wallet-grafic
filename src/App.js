aecaaec921a0aa9966e9339e92356ace262444ef
Douglas Passos <douglas.p.doug@gmail.com>
2023-05-23 20:04:44 -0300

    criacao e uso de Context

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-/* eslint-disable array-callback-return */
+import { useState, useEffect } from 'react';
+import ApiDataContext from './context/ApiDataContext';
 import './App.css';
 import CardStock from './components/CardStock';
 import GraficoData from './components/GraficoData';
 import NavBar from './components/NavBar';
+import getDataTicket from './services/stocks-services';
 
-function App() {  
-  const stockData = {
-    "stock": "PETR3F",
-    "name": "PETROBRAS",
-    "close": 29.18,
-    "change": -0.06849657,
-    "volume": 20544,
-    "market_cap": 364644976258,
-    "logo": "https://s3-symbol-logo.tradingview.com/brasileiro-petrobras--big.svg",
-    "sector": "Energy Minerals"
-  } 
-
-
+ function App() {  
+  
+ const [menuOption, setMenuOption ] = useState(['PETR4']);
+ const [dataApi, setDataApi ] = useState([])
+ 
+ useEffect(() => {
+   if(menuOption !== null) {      
+      const data = getDataTicket(menuOption);         
+      setDataApi(data);
+    }
+  },[menuOption])
+  
+  console.log(dataApi);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <NavBar></NavBar>      
-      </header>
-      <CardStock data={stockData}></CardStock>
-      <GraficoData></GraficoData>
+      <ApiDataContext.Provider value={{menuOption, setMenuOption}}>
+        <header className="App-header">
+          <NavBar></NavBar>      
+        </header>
+        <div className='area-cards'>
+          <CardStock className='card-stock' data={dataApi}></CardStock>       
+        </div>
+        <div className='area-grafic'>
+          <GraficoData></GraficoData>
+        </div>
+      </ApiDataContext.Provider>
     </div>
   );
 }
